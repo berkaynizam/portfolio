a39595bc0eee2a87602b2dc32615f0bce78e74a9
Berkay Nizam <brkynzm@gmail.com>
2023-02-18 16:33:04 +0300

    head google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import Head from 'next/head';
 import Kbar from 'components/Kbar';
 import Header from 'components/Header';
 import Footer from 'components/footer';
+import Script from 'next/script';
 
 export default function MyApp({ Component, pageProps }) {
   return (
@@ -15,6 +16,16 @@ export default function MyApp({ Component, pageProps }) {
               name="viewport"
               content="width=device-width, initial-scale=1"
             />
+              <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=G-J38ZYWZDQ0`} />
+              <Script strategy="lazyOnload">
+                  {`
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments)}
+                  gtag('js', new Date());
+
+                  gtag('config', 'G-J38ZYWZDQ0');
+                  `}
+            </Script>
           </Head>
 
           <Kbar>
