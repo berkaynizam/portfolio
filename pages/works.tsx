bdd500bf2082cfdee68b8dfefc97428a06c0e1ce
Berkay Nizam <brkynzm@gmail.com>
2024-01-16 15:40:26 +0300

    title spacing 3

--- pages/works.tsx
+++ pages/works.tsx
@@ -89,67 +89,69 @@ function PhotosPage({photos, stats}) {
             </div>
 
 
-            <h3 className="text-md mb-5 py-14">eCommerce Websites</h3>
+            <div className="py-14">
+              <h3 className="text-md mb-5">eCommerce Websites</h3>
 
-            <div className="flex flex-col mb-10">
-              <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                  <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                    <table className="min-w-full divide-y divide-gray-200">
-                      <thead className="bg-gray-50">
-                      <tr>
-                        <th scope="col"
-                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                          Project
-                        </th>
-                        <th scope="col"
-                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                          Type
-                        </th>
-                        <th scope="col"
-                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                          Industry
-                        </th>
-                        <th scope="col" className="relative px-6 py-3">
-                          <span className="sr-only">View</span>
-                        </th>
-                      </tr>
-                      </thead>
-                      <tbody className="bg-white divide-y divide-gray-200">
-                      {Etics.map((work, index) => (
-                          <tr>
-                            <td className="px-6 py-4 whitespace-nowrap">
-                              <div className="flex items-center">
-                                <div className="flex-shrink-0 h-10 w-10">
-                                  <a>
-                                    <img src={work.image} className="h-10 w-10 object-cover"
-                                         alt="Work"/></a>
-                                </div>
-                                <div className="ml-4">
-                                  <div className="text-sm font-medium text-gray-900">
-                                    <a href={work.contentLink} target="_blank">{work.title}</a>
+              <div className="flex flex-col mb-10">
+                <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                  <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                    <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                      <table className="min-w-full divide-y divide-gray-200">
+                        <thead className="bg-gray-50">
+                        <tr>
+                          <th scope="col"
+                              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            Project
+                          </th>
+                          <th scope="col"
+                              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            Type
+                          </th>
+                          <th scope="col"
+                              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            Industry
+                          </th>
+                          <th scope="col" className="relative px-6 py-3">
+                            <span className="sr-only">View</span>
+                          </th>
+                        </tr>
+                        </thead>
+                        <tbody className="bg-white divide-y divide-gray-200">
+                        {Etics.map((work, index) => (
+                            <tr>
+                              <td className="px-6 py-4 whitespace-nowrap">
+                                <div className="flex items-center">
+                                  <div className="flex-shrink-0 h-10 w-10">
+                                    <a>
+                                      <img src={work.image} className="h-10 w-10 object-cover"
+                                           alt="Work"/></a>
                                   </div>
-                                  <div className="text-sm text-gray-500">{work.subtitle}</div>
+                                  <div className="ml-4">
+                                    <div className="text-sm font-medium text-gray-900">
+                                      <a href={work.contentLink} target="_blank">{work.title}</a>
+                                    </div>
+                                    <div className="text-sm text-gray-500">{work.subtitle}</div>
+                                  </div>
                                 </div>
-                              </div>
-                            </td>
-                            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                              {work.type}
-                            </td>
-                            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                              {work.clientIndustry}
-                            </td>
-                            <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                              <a href={work.contentLink} target="_blank"
-                                 className="text-indigo-600 hover:text-indigo-900">
-                                View Detail
-                              </a>
-                            </td>
-                          </tr>
-                      ))}
+                              </td>
+                              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                {work.type}
+                              </td>
+                              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                {work.clientIndustry}
+                              </td>
+                              <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                <a href={work.contentLink} target="_blank"
+                                   className="text-indigo-600 hover:text-indigo-900">
+                                  View Detail
+                                </a>
+                              </td>
+                            </tr>
+                        ))}
 
-                      </tbody>
-                    </table>
+                        </tbody>
+                      </table>
+                    </div>
                   </div>
                 </div>
               </div>
